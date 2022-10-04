b57e4e44e402f69ba411037cd4f02ee0153b7649
howCanIFind <second9000@naver.com>
2022-10-04 10:11:45 +0900

    연산

--- 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 1/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 1/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -68,17 +68,24 @@ public class BasicController {
         return "basic/link";
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
             return "Hello " + data;
         }
-    }
-
-    @GetMapping("/literal")
-    public String literal(Model model) {
-        model.addAttribute("data", "Spring!");
-        return "basic/literal";
     }
 
     @Data
