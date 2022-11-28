9cdf011b9b2679d6e4ab47578b0e1e3814610034
howCanIFind <second9000@naver.com>
2022-11-28 11:05:40 +0900

    synchronized 이용해보기

--- 재고시스템으로알아보는동시성해결/src/main/java/com/example/stock/service/StockService.java
+++ 재고시스템으로알아보는동시성해결/src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,7 +14,20 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+    @Transactional
     public void decrease(Long id, Long quantity) {
+        // get stock
+        // 재고 감소
+        // 저장
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @Transactional // transactional 을 끄면 synchronized 로 해결 가능
+    public synchronized void decreaseSynchronized(Long id, Long quantity) {
         // get stock
         // 재고 감소
         // 저장
